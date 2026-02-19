2a67e3d7534c5590985b0704f341b3fec110597a
judare <fosebadgame@gmail.com>
2026-02-19 15:00:34 -0500

    feat: add login first page

--- app/(auth)/login/components/LoginForm.tsx
+++ app/(auth)/login/components/LoginForm.tsx
@@ -1,10 +1,9 @@
 "use client";
-import { useState, useRef } from "react";
+import { useState } from "react";
 import DS from "design-system";
 import { signIn } from "@/app/hooks/useSession";
 import { useApi } from "@/app/hooks/useApi";
-import { fetchForgotPassword } from "@/app/services/auth";
-import { Github, UserCircle } from "lucide-react";
+import { UserCircle } from "lucide-react";
 import { useContextSelector } from "use-context-selector";
 import { Context } from "@/app/providers/Session";
 
@@ -12,16 +11,10 @@ export default function LoginForm() {
   const w: any = typeof window !== "undefined" ? (window as any) : null;
   const searchParams = new URLSearchParams(w?.location.search);
   const [form, setForm]: any = useState({});
-  const modalRecoverPassword: any = useRef(null);
   const showToastConfirm = useContextSelector(
     Context,
     (a) => a.showToastConfirm,
   );
-  const {
-    loading: loadingForgot,
-    errors: errorsForgot,
-    callApi: callApiForgot,
-  } = useApi(fetchForgotPassword);
 
   const { loading, errors, callApi } = useApi(signIn);
 
@@ -35,46 +28,14 @@ export default function LoginForm() {
     });
   };
 
-  const loginThird = (third: string) => {
-    callApi(third, { redirect: searchParams.get("redirect") || "/dashboard" });
-  };
-
-  const handleForgotPassword = () => {
-    callApiForgot(form).then(() => {
-      modalRecoverPassword.current.hideModal();
-    });
-  };
-
   return (
     <div className="container ">
       <div className="lg:max-w-[28rem] relative z-10 mx-auto flex flex-col gap-5">
         <a href="/" className="block ">
-          <img src="/logo.svg" className="h-8 mx-auto " />
+          <img src="/logo.png" className="h-32 mx-auto " />
         </a>
 
         <div className="   px-5 ">
-          <div className="mt-5 grid grid-cols-1 gap-3">
-            {process.env.NEXT_PUBLIC_GOOGLE_LOGIN != "0" && (
-              <div
-                className="login-button"
-                onClick={() => loginThird("google")}
-              >
-                <img src="/logos/google.svg" className="size-5" />
-                <div>Continuar con Google</div>
-              </div>
-            )}
-
-            {process.env.NEXT_PUBLIC_GITHUB_LOGIN != "0" && (
-              <div
-                className="login-button"
-                onClick={() => loginThird("github")}
-              >
-                <Github className="text-neutral-600 size-5" />
-                <div>Continuar con GitHub</div>
-              </div>
-            )}
-          </div>
-
           <div className="relative h-10 w-full my-3">
             <div className="absolute top-3 left-0 w-full justify-center flex z-10">
               <div className="bg-white px-3 text-sm text-neutral-400 ">
@@ -117,64 +78,8 @@ export default function LoginForm() {
               loading={loading}
             />
           </form>
-
-          <div
-            className="text-neutral-600 dark:text-neutral-300 mt-5 font-light text-sm text-center dark:hover:text-white cursor-pointer"
-            onClick={() => modalRecoverPassword.current.showModal()}
-          >
-            ¿Problemas para iniciar sesión?
-          </div>
-
-          <a
-            href="/register"
-            className="shadow text-neutral-700 px-5 py-3 rounded-xl  hover:bg-neutral-200 flex items-center gap-5  justify-between  mt-10 border-glow bg-gradient-to-tr from-white to-blue-100 border border-black/10"
-          >
-            <div>
-              <span className="font-bold">¿No tienes una cuenta? </span>
-              <div className="font-light text-sm">Registrarme</div>
-            </div>
-            <UserCircle className="size-8 text-neutral-700" />
-          </a>
         </div>
       </div>
-
-      <DS.Modal
-        title="Recuperar contraseña"
-        darkMode={true}
-        footer={
-          <div className="flex items-center gap-5">
-            <DS.Button
-              text="Cancelar"
-              onClick={() => modalRecoverPassword.current.hideModal()}
-              variant="secondary"
-              className="w-full"
-              size="md"
-            />
-            <DS.Button
-              text="Confirmar"
-              onClick={() => handleForgotPassword()}
-              variant="primary"
-              className="w-full"
-              size="md"
-              loading={loadingForgot}
-              disabled={!form.email}
-            />
-          </div>
-        }
-        size="sm"
-        ref={modalRecoverPassword}
-      >
-        <DS.Input
-          type="email"
-          value={form.email}
-          label="Email"
-          className="mb-3"
-          error={errorsForgot?.email}
-          onChange={(val: string) => {
-            setForm({ ...form, email: val });
-          }}
-        />
-      </DS.Modal>
     </div>
   );
 }
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,59 +1,15 @@
-import { Header } from "@/app/components/Header";
-import Footer from "@/app/components/Footer";
-import Hero from "./components/Hero";
-import { SessionProvider } from "../providers/Session";
-import { getServerSession } from "@/lib/sessionServer";
+// redirect to login
+"use client";
 
-export const Feature = ({
-  title,
-  description,
-}: {
-  title: string;
-  description: string;
-}) => {
-  return (
-    <div className="w-full">
-      <div className="rounded-2xl bg-neutral-100 w-full h-[300px]"></div>
-      <div className="text-center text-xl font-bold text-neutral-900 mt-5">
-        {title}
-      </div>
-      <p className="text-center text-neutral-600">{description}</p>
-    </div>
-  );
-};
-export default async function Landing() {
-  const session = await getServerSession();
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
-  return (
-    <SessionProvider session={session}>
-      <div className="bg-white text-neutral-600">
-        <div className=" py-3 mx-auto">
-          <div className="relative z-10 min-h-screen pb-10 flex flex-col  ">
-            <Header className="md:h-0" />
+export default function Page() {
+  const router = useRouter();
 
-            <Hero />
-          </div>
+  useEffect(() => {
+    router.push("/login");
+  }, []);
 
-          <div className="flex items-center gap-10 mt-20 mb-20 mx-20 ">
-            <Feature
-              title="Mejora tus anuncios"
-              description="Crear tu propio anuncio"
-            />
-            <Feature
-              title="Mejora tus anuncios"
-              description="Crear tu propio anuncio"
-            />
-            <Feature
-              title="Mejora tus anuncios"
-              description="Crear tu propio anuncio"
-            />
-          </div>
-
-          <div className="">
-            <Footer />
-          </div>
-        </div>
-      </div>
-    </SessionProvider>
-  );
+  return <div>Redirecting...</div>;
 }
