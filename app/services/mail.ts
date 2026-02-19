ce2b3e68ce58e38a950bfc8ef320214a10f74f92
judare <fosebadgame@gmail.com>
2026-02-19 15:09:05 -0500

    feat: remove code

--- app/services/mail.ts
+++ app/services/mail.ts
@@ -3,7 +3,7 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 class Email {
-  private sender = "hi@talkia.co";
+  private sender = "hi@sinergia.co";
 
   async send(to: string, subject: string, component: any, context: any) {
     await resend.emails.send({
