5fcc34dd5f47e22ed9c468d5ee34469ebd783668
judare <fosebadgame@gmail.com>
2026-02-26 15:44:44 -0500

    feat: insert dynamic area requests

--- app/api/onboarding/create/route.ts
+++ app/api/onboarding/create/route.ts
@@ -3,11 +3,15 @@ import { NextResponse } from "next/server";
 import db from "@/db/conn";
 import moment from "moment";
 
-const { OnboardingProcess } = db;
+const { OnboardingProcess, Area, AreaRequest } = db;
 
 export const POST = withUser(async function ({ user, body }: any) {
   let data = body.data;
-  await OnboardingProcess.create({
+
+  const areas = await Area.findAll({
+    where: {},
+  });
+  let onboarding = await OnboardingProcess.create({
     processCode: data.processCode,
     fullName: data.fullName,
     documentType: data.fullName,
@@ -18,6 +22,18 @@ export const POST = withUser(async function ({ user, body }: any) {
     managerId: user.id,
     status: "Pendiente",
   });
+
+  let toInsertRequests = [];
+  for (let area of areas) {
+    toInsertRequests.push({
+      onboardingProcessId: onboarding.id,
+      areaId: area.id,
+      status: "Pendiente",
+      deadline: moment().add(1, "month").format("YYYY-MM-DD"),
+    });
+  }
+
+  await AreaRequest.bulkCreate(toInsertRequests);
 
   return NextResponse.json({
     data: {},
