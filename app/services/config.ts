c4395433e042830583a83afe4f98e19848e96c59
judare <fosebadgame@gmail.com>
2026-03-04 17:53:04 -0500

    feat: remove wss var

--- app/services/config.ts
+++ app/services/config.ts
@@ -2,7 +2,6 @@
 
 export const VERSION = "1.0.1";
 
-let wss = "talkia.co";
 let staticPath = "https://static.blokay.com/talkia";
 let baseUrl = `https://talkia.co`;
 let url = `${baseUrl}/api/`;
@@ -10,12 +9,9 @@ let url = `${baseUrl}/api/`;
 if (typeof process !== "undefined") {
   staticPath = "";
   baseUrl = process.env.NEXT_PUBLIC_URL || "";
-  wss = process.env.NEXT_PUBLIC_WSS || "";
   url = process.env.NEXT_PUBLIC_API || "";
 }
 export const BASE_URL = baseUrl;
-
-export const WSS = wss;
 
 export const API = url;
 
